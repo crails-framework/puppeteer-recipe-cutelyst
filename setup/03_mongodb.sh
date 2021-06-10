b4d142e70495135b221f627a988c4b3c2cfb9c24
Michael <michael@unetresgrossebite.com>
2021-06-11 00:39:56 +0200

    removed mongodb user directory

--- setup/03_mongodb.sh
+++ setup/03_mongodb.sh
@@ -11,6 +11,3 @@ echo "deb [ arch=amd64,arm64 ] $repository" > /etc/apt/sources.list.d/mongodb-or
 
 apt-get update
 apt-get install -y mongodb-org
-
-mkdir -p /home/$APP_USER/db
-chown -R $APP_USER:$APP_USER /home/$APP_USER/db
