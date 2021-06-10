9f475998088510799937b0da60eddee924eb74dd
Michael <michael@unetresgrossebite.com>
2021-06-11 01:02:50 +0200

    fixed logreader crashing

--- logreader.sh
+++ logreader.sh
@@ -7,7 +7,7 @@ total_lines=`wc -l "$logfile" | cut -d' ' -f1`
 extract_lines=500
 
 if [[ "$LAST_LOG_LINE" > "$total_lines" ]] ; then
-  extract_lines=`$total_lines`
+  extract_lines="$total_lines"
 elif [[ "$LAST_LOG_LINE" > 0 ]] ; then
   extract_lines=`expr $total_lines - $LAST_LOG_LINE`
 fi
